c7be1b762434bea73d0eb24da99b2321501bd4cb
Marcus Kida <marcus.kida@gmail.com>
2013-01-18 19:17:37 +0100

    Initial Commit

---
+++ tapebooth/ICAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ICAppDelegate.h
+//  tapebooth
+//
+//  Created by Marcus Kida on 18.01.13.
+//  Copyright (c) 2013 Marcus Kida [indiecoder.net]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ tapebooth/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tapebooth
+//
+//  Created by Marcus Kida on 18.01.13.
+//  Copyright (c) 2013 Marcus Kida [indiecoder.net]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+    }
+}
