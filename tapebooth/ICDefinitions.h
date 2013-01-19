3b00ad05d0157af5c90c212311a45ec3b90015fa
Marcus Kida <marcus.kida@gmail.com>
2013-01-19 04:05:53 +0100

    Removed some statements in Definitions, this project will be quick and dirty

--- tapebooth/ICDefinitions.h
+++ tapebooth/ICDefinitions.h
@@ -9,11 +9,7 @@
 // OAuth
 #define kOAuthAppId                 @"b045e353-77e3-496f-b851-08b95c5e9cf3"
 #define kOAuthApiAuthUrl            @"https://api.doctape.com/oauth2"
-#define kOAuthApiTokenUrl           @"https://api.doctape.com/oauth2/token"
 #define kOAuthRedirectUrl           @"urn:ietf:wg:oauth:2.0:oob"
-
-// Segues
-#define kSegueShowWebView           @"ShowWebView"
 
 // UserDefaults Keys
 #define kAuthTokenKey               @"authToken"
